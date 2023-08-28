684bc33951cd7691a86202e66a9910b97ac2cdfc
petarhlebaroff <petarhlebaroff@gmail.com>
2023-08-28 16:10:35 +0300

    Initial commit

---
+++ src/test/java/core/BaseTest.java
@@ -0,0 +1,76 @@
+package core;
+
+import org.example.BrowserTypes;
+import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @AfterEach
+    public void afterEachTestInAClass(){
+        // close driver
+        driver.close();
+    }
+
+    protected static WebDriver startBrowser(BrowserTypes browserType) {
+        // Setup Browser
+        switch (browserType){
+            case CHROME:
+                ChromeOptions chromeOptions = new ChromeOptions();
+                return new ChromeDriver(chromeOptions);
+            case FIREFOX:
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                return new FirefoxDriver(firefoxOptions);
+            case EDGE:
+                EdgeOptions edgeOptions = new EdgeOptions();
+                return new EdgeDriver(edgeOptions);
+        }
+
+        return null;
+    }
+
+    protected static void authenticateWithUser(String username, String pass) {
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@data-test='username']"));
+        usernameInput.sendKeys(username);
+
+        WebElement password = driver.findElement(By.xpath("//input[@data-test='password']"));
+        password.sendKeys(pass);
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@data-test='login-button']"));
+        loginButton.click();
+
+        WebElement inventoryPageTitle = driver.findElement(By.xpath("//div[@class='app_logo']"));
+        wait.until(ExpectedConditions.visibilityOf(inventoryPageTitle));
+    }
+
+    protected WebElement getProductByTitle(String title){
+        return driver.findElement(By.xpath(String.format("//div[@class='inventory_item' and descendant::div[text()='%s']]", title)));
+    }
+
+    protected static void fillShippingDetails(String firstName, String lastName, String zip) {
+        driver.findElement(By.id("first-name")).sendKeys(firstName);
+        driver.findElement(By.id("last-name")).sendKeys(lastName);
+        driver.findElement(By.id("postal-code")).sendKeys(zip);
+    }
+
+    protected static void refreshSauceDemoWebsite() {
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("reset_sidebar_link")));
+        driver.findElement(By.id("reset_sidebar_link")).click();
+        driver.findElement(By.id("logout_sidebar_link")).click();
+    }
+
+}
---
+++ src/test/java/sausedemotests/LoginTests.java
@@ -0,0 +1,165 @@
+package sausedemotests;
+
+import core.BaseTest;
+import org.example.BrowserTypes;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.time.Duration;
+import java.util.List;
+import java.util.Locale;
+
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class LoginTests extends BaseTest {
+
+    @BeforeEach
+    public void beforeAllTests(){
+        driver = startBrowser(BrowserTypes.CHROME);
+
+        // Configure wait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        // Navigate to Google.com
+        driver.get("https://www.saucedemo.com/");
+
+        authenticateWithUser("standard_user", "secret_sauce");
+    }
+
+    @Test
+    @Order(1)
+    public void userAuthenticated_when_validCredentialsProvided(){
+
+
+        // Add Assert
+        WebElement inventoryPageTitle = driver.findElement(By.xpath("//div[@class='app_logo']"));
+        wait.until(ExpectedConditions.visibilityOf(inventoryPageTitle));
+
+        Assertions.assertEquals("Swag Labs", inventoryPageTitle.getText(), "The login is not successful");
+
+//        refreshSauceDemoWebsite();
+    }
+
+    @Test
+    @Order(2)
+    public void productAddedToShoppingCar_when_addToCart(){
+        // Add Backpack and T-shirt to shopping cart
+        String backpackTitle = "Sauce Labs Backpack";
+        String shirtTitle = "Sauce Labs Bolt T-Shirt";
+
+        WebElement backpack = getProductByTitle(backpackTitle);
+        backpack.findElement(By.className("btn_inventory")).click();
+
+        var tshirt = getProductByTitle(shirtTitle);
+        tshirt.findElement(By.className("btn_inventory")).click();
+
+        driver.findElement(By.className("shopping_cart_link")).click();
+
+        // Click on shopping Cart
+        driver.findElement(By.className("shopping_cart_link")).click();
+
+        // Assert Items and Totals
+        var items = driver.findElements(By.className("inventory_item_name"));
+
+        Assertions.assertEquals(2, items.size(), "Items count not as expected");
+
+        Assertions.assertEquals(backpackTitle, items.get(0).getText(), "Item title not as expected");
+        Assertions.assertEquals(shirtTitle, items.get(1).getText(), "Item title not as expected");
+
+//        refreshSauceDemoWebsite();
+    }
+
+    @Test
+    @Order(3)
+    public void userDetailsAdded_when_checkoutWithValidInformation(){
+        // Add Backpack and T-shirt to shopping cart
+        String backpackTitle = "Sauce Labs Backpack";
+        String shirtTitle = "Sauce Labs Bolt T-Shirt";
+
+        var backpack = getProductByTitle(backpackTitle);
+        backpack.findElement(By.className("btn_inventory")).click();
+
+        var tshirt = getProductByTitle(shirtTitle);
+        tshirt.findElement(By.className("btn_inventory")).click();
+
+        // Click on shopping Cart
+        driver.findElement(By.className("shopping_cart_link")).click();
+
+        // Fill Contact Details
+        driver.findElement(By.id("checkout")).click();
+
+        fillShippingDetails("FirstName", "LastName", "zip Code");
+        driver.findElement(By.id("continue")).click();
+
+        // Assert Details in next step
+        WebElement checkoutOverview = driver.findElement(By.xpath("//span[@class='title']"));
+        Assertions.assertEquals("Checkout: Overview", checkoutOverview.getText(),
+                "Checkout is not successful");
+
+        var items = driver.findElements(By.className("inventory_item_name"));
+        var total = driver.findElement(By.className("summary_total_label")).getText();
+        double expectedPrice = 15.99 + 29.99 + 3.68;
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        symbols.setDecimalSeparator('.');
+        symbols.setGroupingSeparator(',');
+        DecimalFormat decimalFormat = new DecimalFormat("###,##0.00", symbols);
+
+        var expectedTotal = "Total: $" + decimalFormat.format(expectedPrice);
+
+        Assertions.assertEquals(2, items.size(), "Items count not as expected");
+
+        Assertions.assertEquals(backpackTitle, items.get(0).getText(), "Item title not as expected");
+        Assertions.assertEquals(shirtTitle, items.get(1).getText(), "Item title not as expected");
+        Assertions.assertEquals(expectedTotal, total, "Items total price not as expected");
+
+//        refreshSauceDemoWebsite();
+    }
+
+
+    @Test
+    @Order(4)
+    public void orderCompleted_when_addProduct_and_checkout_withConfirm(){
+        // Add Backpack and T-shirt to shopping cart
+        String backpackTitle = "Sauce Labs Backpack";
+        String shirtTitle = "Sauce Labs Bolt T-Shirt";
+
+        var backpack = getProductByTitle(backpackTitle);
+        backpack.findElement(By.className("btn_inventory")).click();
+
+        var tshirt = getProductByTitle(shirtTitle);
+        tshirt.findElement(By.className("btn_inventory")).click();
+
+        // Click on shopping Cart
+        driver.findElement(By.className("shopping_cart_link")).click();
+
+        // Fill Contact Details
+        driver.findElement(By.id("checkout")).click();
+
+        fillShippingDetails("FirstName", "LastName", "zip Code");
+        driver.findElement(By.id("continue")).click();
+
+        // Complete Order
+        driver.findElement(By.id("finish")).click();
+
+        // Assert Complete Message
+        WebElement checkoutCompleteText = driver.findElement(By.xpath("//span[@class='title']"));
+        Assertions.assertEquals("Checkout: Complete!", checkoutCompleteText.getText(),
+                "Checkout is not successful");
+
+        // Assert Items removed from Shopping Cart
+        driver.findElement(By.className("shopping_cart_link")).click();
+
+        List<WebElement> cartBadge = driver.findElements(By.className("shopping_cart_badge"));
+        if (cartBadge.isEmpty()) {
+            Assertions.assertTrue(true, "Shopping cart is empty");
+        } else {
+            Assertions.fail("Shopping cart is not empty");
+        }
+
+    }
+}
